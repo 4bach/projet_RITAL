22869174c8541a7abed34e6df9587324b8801549
marc <mr.treu@gmail.com>
2019-04-28 16:26:38 +0200

    Ajout de commentaire pour buildDocCollectionSimple

--- projet_RI/source/Parser.py
+++ projet_RI/source/Parser.py
@@ -5,36 +5,49 @@ from Document import Document
 class Parser:
     
     def buildDocCollectionSimple(fichier):
-    
+        """
+            Construit l'index a partir d'une base de documents contenus dans fichier,
+        On lit les lignes du fichier une par une, et on repère les balises texte .T
+        et les balises id .I
+
+        :type fichier: String
+        :param fichier: Le fichier qui contient les documents que l'on veut indexé
+
+        :return: Un dictionnaire de d'object Document, dont les clef sont les id des
+                Document.
+                {"id1": Document1, "id2": Document2, ...}
+        """
+
         resultat = dict()
         lireID = ""
-        text =""
-        inT = False
+        text = ""
+        inT = False  # Boolean qui nous indique si l'on est dans une balise texte
         d = Document()
-        f = open(fichier,'r')
-        for l in f.readlines():
+        f = open(fichier, 'r')
+        for l in f.readlines():  # Pour chaque ligne du fichier
             
-            if inT:
-                if l.startswith("."):
-                    inT = False
-                    d.setTexte(text)
-                    resultat[lireID[:-1]] = d
+            if inT:  # Si on est déjà dans une balise de texte
+
+                if l.startswith("."):  # Et Si la ligne courante commence par un .
+                    inT = False                # Alors cela signifie que l'on sort de la texte
+                    d.setTexte(text)           # On va ajouter le texte que l'on a trouver dans notre document
+                    resultat[lireID[:-1]] = d  # On ajoute notre document a notre dictionnaire resultat
                     lireID = ""
-                else:
+
+                else:                  # Sinon, c'est que l'on toujour dans notre balise texte
                     text = text + l 
                 
-            if len(lireID)>0:
-                if l.startswith(".T"):
-                    inT = True
-                    text =""
+            if len(lireID) > 0:  # Si a deja lu une balise id, mais pas encore de balise texte
+                if l.startswith(".T"):  # Et que la ligne courante commence par un .T
+                    inT = True  # alors on est dans une balise texte
+                    text = ""
                 
-            if l[:2] == ".I":
+            if l[:2] == ".I":  # Si on a pas vue de balise id et que la ligne courante commence par .I
                 lireID = l[3:]
-                d = Document()
-                d.setID(int(lireID))
+                d = Document()  # On cree un nouveau document
+                d.setID(int(lireID))  # Auquel on lui donne son id
                 resultat[lireID[:-1]] = ""
-            
-    
+
         f.close()
         return resultat
 
