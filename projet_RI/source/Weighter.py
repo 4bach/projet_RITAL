27c3518d4da683ed26e3378a782495b6ec85f8ce
marc <mr.treu@gmail.com>
2019-04-30 13:27:52 +0200

    Implementation de Weighter4, ajout de commentaire

--- projet_RI/source/Weighter.py
+++ projet_RI/source/Weighter.py
@@ -1,151 +1,95 @@
 from collections import Counter
 from TextRepresenter import PorterStemmer
 import math
+import IndexerSimple
 import numpy as np
 
 
 class Weighter:
-    
-    def __init__(self,indexer):
+
+    def __init__(self, indexer):
         self.indexer = indexer
-        
-        
-    def getWeightsForDoc(self,idDoc):
-        pass
-        
-    
-    def getWeightsForStem(self,stem):
-        pass
-    
+
+    def getWeightsForDoc(self, idDoc):
+        return self.indexer.getTfsForDoc(idDoc)
+
+    def getWeightsForStem(self, stem):
+        return self.indexer.getTfIDFsForStem(stem)
+
     def getNbDoc(self):
+        return self.indexer.getNbDoc()
+
+    def getWeightsForQuery(self, query):
         pass
-        
-    def getWeightsForQuery(self,query):
-        pass
-    
-    
-        
-    
+
 
 class Weighter1(Weighter):
-    
-    def __init__(self,indexer):
-        super().__init__(indexer)
-        
-        
-    def getWeightsForDoc(self,idDoc):
-        return self.indexer.getTfsForDoc(idDoc)
-    
-    def getWeightsForStem(self,stem):
-        return self.indexer.getTfIDFsForStem(stem)
-    
+    """
+        Premier schéma pour la ponderation des documents et des query.
 
-    def getWeightsForQuery(self,query): 
-        ps = PorterStemmer()
-        q_count=ps.getTextRepresentation(query)
-        res=dict()
-        for item in self.indexer.index_inv.keys():
-            if item in q_count.keys():
-                res[item]=1
-#            else:
-#                res[item]=0
-        return res
-    
-    def getNbDoc(self):
-        return self.indexer.getNbDoc()
-        
-        
-        
-        """
-        qry = [porter.stem(i.lower()) for i in query.split() if i.lower() not in self.indexer.motvide]
-        score = dict()
-        for i in range(self.indexer.getNbDoc()):
-            score[i]=0
-            for j in qry:
-                if j in self.indexer.getTfsForDoc(i):
-                    score[i]+=  1
-            if len(qry)==score[i]:
-                score[i] = 1 
-            else:
-                score[i] = 0
-        return score
-        """
-        
-    
-        
-    
+    poid doc = tf
+    poid query = 1 if terme in query else 0
+    """
+
+    def __init__(self, indexer):
+        super().__init__(indexer)
+
+    def getWeightsForQuery(self, query):
+        return {terme: 1 for terme in self.indexer.countWord(query)}
+
+
 class Weighter2(Weighter):
-    
-    def __init__(self,indexer):
+    """
+        Deuxième schéma pour la ponderation des documents et des query.
+
+    poid doc = tf
+    poid query = tf
+    """
+    def __init__(self, indexer):
         super().__init__(indexer)
-        
-        
-    def getWeightsForDoc(self,idDoc):
-        return self.indexer.getTfsForDoc(idDoc)
-    
-    def getWeightsForStem(self,stem):
-        return self.indexer.getTfIDFsForStem(stem)
-    
-    def getNbDoc(self):
-        return self.indexer.getNbDoc()
 
-    def getWeightsForQuery(self,query):  
-        ps = PorterStemmer()
-        
-        q_count=ps.getTextRepresentation(query)
-        poids=dict()
-        for item in self.indexer.index_inv.keys():
-            if item in q_count.keys():
-                poids[item]=q_count[item]
-#            else:
-#                res[item]=0
-        return poids
-
+    def getWeightsForQuery(self, query):
+        query_rep = self.indexer.countWord(query)
+        taille_query_rep = sum([query_rep[n] for n in query_rep])
+        return {terme: (query_rep[terme]/taille_query_rep) for terme in query_rep}
 
 
 class Weighter3(Weighter):
-      
-    def __init__(self,indexer):
-        super().__init__(indexer)
-        
-        
-    def getWeightsForDoc(self,idDoc):
-        return self.indexer.getTfsForDoc(idDoc)
-    
-    def getWeightsForStem(self,stem):
-        return self.indexer.getTfIDFsForStem(stem)
-        
-    def getNbDoc(self):
-        return self.indexer.getNbDoc()
+    """
+        troisième schéma pour la ponderation des documents et des query.
 
-    def getWeightsForQuery(self,query): 
-        ps = PorterStemmer()
-         
-        q_count=ps.getTextRepresentation(query)
-        poids=dict()
-        for item in self.indexer.index_inv.keys():
-            if item in q_count.keys():
-                poids[item]=np.log((self.indexer.getNbDoc()+1)/(1+len(self.indexer.getTfsForStem(item))))
-#            else:
-#                res[item]=0
-        return poids
-        
+    poid doc = tf
+    poid query = idf if terme in query else 0
+    """
+    def __init__(self, indexer):
+        super().__init__(indexer)
+
+    def getWeightsForQuery(self, query):
+
+        resultat = dict()
+        for terme in self.indexer.countWord(query):
+            if terme in self.indexer.index_inv:
+                resultat[terme] = math.log((1 + self.indexer.getNbDoc()) / (1 + len(self.indexer.index_inv[terme])))
+        return resultat
 
 
 class Weighter4(Weighter):
-    pass
+
+    def getWeightsForDoc(self, idDoc):
+        tf_doc = self.indexer.getTfsForDoc(idDoc)
+        return {terme: 1 + math.log(tf_doc[terme]) for terme in tf_doc}
+
+    def getWeightsForStem(self, stem):
+        tf_stem = self.indexer.getTfIDFsForStem(stem)
+        return {terme: 1 + math.log(tf_stem[terme]) for terme in tf_stem}
+
+    def getWeightsForQuery(self, query):
+
+        resultat = dict()
+        for terme in self.indexer.countWord(query):
+            if terme in self.indexer.index_inv:
+                resultat[terme] = math.log((1 + self.indexer.getNbDoc()) / (1 + len(self.indexer.index_inv[terme])))
+        return resultat
 
 class Weighter5(Weighter):
     pass
-        
-
-
-
-
-
-
-
-
-
-
-            
